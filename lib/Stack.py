330ca9ae17327c651d28a5fc928f4cfe50b02c2e
Ej Myles <ejelymyles@gmail.com>
2023-11-21 17:14:05 -0800

    finish lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,49 @@
+import ipdb 
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+        
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+        
 
     def push(self, item):
-        pass
-
+        if len(self.items) != self.limit:
+            self.items.append(item)
+    
+           
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
+        
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
+        
 
     def full(self):
-        pass
-
+        return self.size() >= self.limit
+       
+        
     def search(self, target):
-        pass
+        distance = self.size()
+        for value in self.items:
+            # ipdb.set_trace()
+            if value != target:
+                distance -= 1
+            else:
+                return distance - 1
+
+        return -1
+        
